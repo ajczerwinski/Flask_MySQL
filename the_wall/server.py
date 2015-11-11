b5423063013629051182ecb4cbb4cb3bdce7f92d
Allen Czerwinski <allen.czerwinski@gmail.com>
2015-11-10 20:18:24 -0800

    first commit with login, registration, and message box

--- the_wall/server.py
+++ the_wall/server.py
@@ -9,14 +9,18 @@ import time
 EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-mysql = MySQLConnector('login_registration')
+mysql = MySQLConnector('the_wall')
 app.secret_key = "SecretKeyMePls"
 
 @app.route('/')
 def index():
-	users = mysql.fetch("SELECT * FROM users")
-	print users
-	return render_template('index.html', users=users)
+	if 'user_id' in session:
+			return render_template('wall.html')
+	else: 
+		users = mysql.fetch("SELECT * FROM users")
+		messages = mysql.fetch("SELECT * FROM messages")
+		comments = mysql.fetch("SELECT * FROM comments")
+	return render_template('index.html')
 
 @app.route('/register', methods=['POST'])
 def create():
@@ -24,8 +28,8 @@ def create():
 	last_name = request.form['last_name']
 	email = request.form['email']
 	password = request.form['password']
+	confirm_password = request.form['confirm_password']
 
-	# run validations and if they are successful, create password hash with bcrypt
 	if len(first_name) < 2 or len(last_name) < 2:
 		flash("First and last name must be at least 2 letters!")
 		return redirect('/')
@@ -35,24 +39,25 @@ def create():
 	elif not EMAIL_REGEX.match(email):
 		flash("Invalid Email Address")
 		return redirect('/')
-	# Need to check to see if email already exists in DB
+	# TODO - add logic to check if email already exists in database
 	elif len(password) < 9:
 		flash("Password must have more than 8 characters!")
+		return redirect('/')
+	elif password != confirm_password:
+		flash("Password doesn't match the password confirmation, try again!")
 		return redirect('/')
 	else:
 		pw_hash = bcrypt.generate_password_hash(password)
 		password = pw_hash
-		# print password
-		query = "INSERT INTO users (first_name, last_name, email, password, created_at) VALUES ('{}', '{}', '{}', '{}', NOW())".format(first_name, last_name, email, password)
-		print "Below me is the user query"
+		query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES ('{}', '{}', '{}', '{}', NOW(), NOW())".format(first_name, last_name, email, password)
 		mysql.run_mysql_query(query)
 		user_query = "SELECT * FROM users WHERE email = '{}' LIMIT 1".format(email)
 		user = mysql.fetch(user_query)
 		print user
 		session['user'] = user[0]
 		session_user_id = session['user']
-		# print "Above me is the session user ID index 0!!"
-		return render_template('success.html', session_user_id=session_user_id)
+		return render_template('wall.html', session_user_id=session_user_id)
+
 @app.route('/login', methods=['POST'])
 def login():
 	email = request.form['email']
@@ -64,12 +69,14 @@ def login():
 	if bcrypt.check_password_hash(decoded_password, str(password)):
 		session['user'] = user[0]
 		session_user_id = session['user']
-		return render_template('success.html', session_user_id=session_user_id)
+		return render_template('wall.html', session_user_id=session_user_id)
 	else:
 		flash("Email and password don't match. Try again!")
 		return redirect('/')
+
 @app.route('/logout', methods=['POST'])
 def logout():
 	session.clear()
 	return redirect('/')
+
 app.run(debug=True)
